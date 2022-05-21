52f635c92ff3cc58bdad8377fb24395e5cb17ebf
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-05-21 22:48:45 +0600

    all the pages are set but the route is not set

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>this is my 12 number assignment</h1>
-      <button class="btn w-64 rounded-full">Button </button>
+
     </div>
   );
 }
---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            blogs
+        </div>
+    );
+};
+
+export default Blogs;
